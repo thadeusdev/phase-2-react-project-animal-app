f4e7f4f044bcf7f856eb6211bb02a825cc18000d
thadeus <thadeusnyariki94@gmail.com>
2022-10-03 18:39:20 +0300

    display animals and pass props

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-function AnimalCard() {
+function AnimalCard({animal}) {
   return (
-    <div>AnimalCard</div>
+    <div>
+        <div>
+            {/* <img src={animal.image_link} /> */}
+        </div> 
+        <div>
+          <h3>{animal.name}</h3>
+          <p>{animal.animal_type}</p>
+        </div>
+        <div>
+          <button>Donate</button>
+          <button>Adopt</button>
+        </div>       
+    </div>
+    
   )
 }
 
--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import AnimalCard from './AnimalCard'
 import Forms from './Forms'
 
+const baseUrl = 'https://zoo-animal-api.herokuapp.com/animals/rand/10'
+
 function AnimalList() {
+    const [animals, setAnimals] = useState([])
+
+    useEffect(() => {
+        fetch(baseUrl)
+        .then(res => res.json())
+        .then(animals => setAnimals(animals))
+    }, [])
   return (
     <div>
         <Forms />
-        <AnimalCard />
+        {animals.map((animalObj) => (
+            <AnimalCard key={animalObj.id} animal={animalObj} />
+        ))}
     </div>
   )
 }
